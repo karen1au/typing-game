914588ea0dd7c5d8d7a3db1c41b5e01edd3b6147
karen1au <kagemikaka@hotmail.com>
2019-07-24 22:08:24 -0400

    fixed timer bug

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -3,27 +3,27 @@ import GameContext from "../game-context";
 import LoadingContext from "../loading-context";
 
 const Timer = props => {
-  const [currentTime, setTimer] = useState(60);
+  const [currentTime, setTimer] = useState(3);
 
-  const { setGameDone } = useContext(GameContext);
+  const { gameDone, setGameDone } = useContext(GameContext);
   const { loading, setLoading } = useContext(LoadingContext);
   
   const timer = () => setTimer(currentTime - 1);
   
   useEffect(() => {
+    if (currentTime < 0) {
+      setGameDone(true);
+      return;
+    }
     if (!loading) {
       const interval = setInterval(timer, 1000);
       return () => clearInterval(interval);
-    }
-    if (currentTime <= 0) {
-      setGameDone(true);
-      return;
     }
   }, [currentTime, loading]);
 
   return (
     <div className="timer-container">
-      {currentTime ? <h1>YOU HAVE {currentTime} seconds</h1> : null}
+      {!gameDone ? <h1>YOU HAVE {currentTime} seconds</h1> : null}
     </div>
   );
 };
